360b5768819ef8d53d6650f0d357278ff5419d53
RafalM2000 <rfm@europe.com>
2021-07-23 13:09:02 +0200

    store jako jedyne źródło prawdy.

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from '../app.component';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styles: [`$childHeaderColor: #ff00e0;
+    h3 { color: $childHeaderColor }`]
 })
 export class ChildComponent implements OnInit {
+  counterJunior$: Observable<number>;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {
+  this.counterJunior$ = store.select('counter');
+}
 
   ngOnInit(): void {
+    setInterval(() => this.store.dispatch({type: 'ZERO'}), 5000);
   }
 
 }
--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -5,6 +5,7 @@ export function counterReducer(state = 0, action: Action): number {
     switch (action.type) {
         case 'PLUS': return state + 1;
         case 'MINUS': return state - 1;
+        case 'ZERO': return state = 0;
         default: return state;
     }
 }
