b01e1d42db0d05ebebeea676b178cdd5ff53d5a5
RafalM2000 <rfm@europe.com>
2021-07-29 17:22:06 +0200

    zmiana nazwy na mycounterReducer

--- src/app/reducer.ts
+++ src/app/reducer.ts
@@ -3,14 +3,12 @@ import { plus, minus, zero } from './counter.actions';
 
 export const initialState = 0;
 
-const _counterReducer = createReducer(
-    initialState, 
-    on(plus, (state) => state +1),
-    on(minus, (state) => state -1),
+const mycounterReducer = createReducer(
+    initialState,
+    on(plus, (state) => state + 1),
+    on(minus, (state) => state - 1),
     on(zero, (state) => state = 0)
     );
-    
-    export function counterReducer(state, action) {
-        return _counterReducer(state, action);
+export function counterReducer(state, action): any {
+        return mycounterReducer(state, action);
       }
-    
